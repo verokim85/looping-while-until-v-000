42c138e3bd4c7a58cabced30d5e5f98e4c108190
Veronica Kim <vero.a.kim@gmail.com>
2017-12-21 15:38:51 +0000

    automatically backed up by learn

--- spec/until_spec.rb
+++ spec/until_spec.rb
@@ -1,9 +1,8 @@
 require_relative "spec_helper"
 require_relative "../lib/until.rb"
 
-describe "#using_until" do
-	it "puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10" do 
-	  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-	  expect{ using_until }.to output(looping_string).to_stdout
-  end
+levitation_force = 6
+until levitation_force <= 10
+	puts "Wingardium Leviosa"
+	levitation_force += 1
 end
--- spec/while_spec.rb
+++ spec/while_spec.rb
@@ -1,9 +1,8 @@
 require_relative "spec_helper"
 require_relative "../lib/while.rb"
 
-describe "#using_while" do 
-	it "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10" do 
-	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-	expect{ using_while }.to output(looping_string).to_stdout
-	end
+levitation_force = 6
+while levitation_force <= 10
+	puts "Wingardium Leviosa"
+	levitation_force += 1
 end
